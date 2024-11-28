408eb78d5c575074de4edc055717b001cd31638d
Wilson F <wilfer.wil@gmail.com>
2024-11-27 23:21:44 -0300

    cc

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -34,13 +34,11 @@ class UserController extends Controller
 
         return redirect()->route('register.form')->with('success', 'Usuário registrado com sucesso!');
     }
-    
+
     public function listTransportadoras()
     {
-        // Fetch only users of type 'transportadora'
         $transportadoras = User::where('account_type', 'transportadora')->get();
 
-        // Pass the data to the view
         return view('users.transportadoras', ['transportadoras' => $transportadoras]);
     }
 }
